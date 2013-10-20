f65f5620df09e8b4f139fd2cc8c7cd2953320e1e
testaccordent <ling.chang@polycom.com>
2013-10-20 12:09:27 -0700

    added unit tests

---
+++ test/unit/sample.tests.js
@@ -0,0 +1,15 @@
+﻿(function () {
+    QUnit.config.testTimeout = 10000;
+    
+    var okAsync = QUnit.okAsync,
+        stringformat = QUnit.stringformat;
+    
+ 
+    module('hello tests');
+
+    test("hello test", 
+        function () {
+            ok(1 == "1", "Passed!");
+        }
+    );
+})();
